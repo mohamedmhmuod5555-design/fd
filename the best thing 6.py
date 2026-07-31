19a96437def22936c84d968118c6431bb33cd63b
mohamedmhmuod5555-design <mohamedmhmuod5555@gmail.com>
2026-07-31 02:18:31 -0700

    Update the best thing 6.py

--- the best thing 6.py
+++ the best thing 6.py
@@ -31,7 +31,7 @@ if sign=='*':
  sc=num1*num2
 if sign=='/':
  sc=num1/num2
- round(sc,2)
+ sc=round(sc,2)
 st.title("Welcome to Math Game ")
 st.write(num1,sign,num2)
 number=st.number_input("ادخل النتيجه ")
